d3e560a7a154e174e62e91a58dadbfe51f8956af
luiz-hms <luiz.henriquems@hotmail.com>
2025-06-20 14:50:06 -0300

    ...

--- mecanix_app/android/app/src/main/kotlin/com/example/mecanix_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mecanix_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mecanix_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mecanix_app/app.dart';
-
-void main() {
-  runApp(const App());
-}
